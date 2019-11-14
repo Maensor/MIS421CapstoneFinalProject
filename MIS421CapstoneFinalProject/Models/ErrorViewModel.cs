ef316a5bc52276a78cd379c622171aa10c539618
Mary Ensor <maensor@crimson.ua.edu>
2019-11-14 14:14:44 -0600

    Comment

---
+++ MIS421CapstoneFinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MIS421CapstoneFinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
